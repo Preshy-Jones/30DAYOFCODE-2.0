c56165cc59b74dded6714eade718f13812720872
Precious Adedibu <40187926+Preshy-Jones@users.noreply.github.com>
2020-05-02 01:18:15 +0100

    updated with documentation

--- day1beg.js
+++ day1beg.js
@@ -1,11 +1,20 @@
 function divisors(integer) {
+    //creating empty array
     db = []
+    //looping from 1 to less than the input integer
     for (var i = 1; i < integer; i++) {
+        //dividing the input integer by each number in the loop
         var div = integer / i;
+        //checking that the result is not a decimal to get the numbers that are perfect divisors
         var result = (div - Math.floor(div)) == 0;
         if (result) {
+            //pushing the divisors into the created array
             db.push(i);
         }
     }
+    //returning the number of divisors by getting the length of the array
     return db.length;
 }
+
+console.log(divisors(10))// 3
+console.log(divisors(20));//5
